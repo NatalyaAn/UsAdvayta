22a07fa4edf82e2d8168aeac121fb02ea1811ba8
Natalya <natalya@NATALYA>
2017-10-16 16:22:32 -0400

    Added books images and descriptions

--- views/books.php
+++ views/books.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+<!--<head>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -28,8 +28,38 @@
     <link href="../public/css/mystyles.css" rel="stylesheet">
     <link href="../js/carousel.js" rel="stylesheet">
 
-</head>
 
+
+
+
+</head>-->
+
+<head>
+    <!-- Latest compiled and minified Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+    <!-- jQuery library -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+    <!-- Popper -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+
+    <!-- Latest compiled and minified Bootstrap JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+    <!-- Initialize Bootstrap functionality -->
+    <script>
+        // Initialize tooltip component
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        })
+
+        // Initialize popover component
+        $(function () {
+            $('[data-toggle="popover"]').popover()
+        })
+    </script>
+</head>
 <body >
 
 <!-- Navbar -->
@@ -42,53 +72,148 @@ include("layout/nav.php");
 
     <header class="text-center"><h3 >Книги</h3></header>
 
+
     <div class="  container py-3 bg-faded">
     <div class="card-deck">
         <div class="card ">
-            <img class="card-img-top book-shadow" src="../public/images/kodeksMastera.jpg" alt="Card image cap">
-            <div class=" card-body ">
-                <p class="card-text"><small>
-                This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-                                    </small></p>
-            </div>
-        </div>
+            <img class="card-img-top book-shadow book-size" src="../public/images/books/000005.jpg" alt="Card image cap">
+            <div class="card-block">
+                <p class="card-text" ><small></small></p>
+                <button type="button" class="btn btn-link" data-toggle="popover"
+                        title="Освобождающий нектар драгоценных наставлений. Лайя Амрита Упадеша Чинтамани"
+                        data-content="Данный текст является бесценным собранием устных наставлений великих святых, сиддхов – существ, достигших вершин духовной реализации.
+                         Он дает духовное пробуждение только слушающим его, поскольку является выражением живой передачи, пробужденного ума – «Гухья мандалой» – тайным
+                          пространством ума, явленным из измерения «чистого видения» богов и богинь. По традиции, такие тексты передаются либо в безмолвии, либо особыми
+                           словами, либо в сновидениях, посредством «тонкой речи» от учителя к ученику. Само по себе издание подобного текста указывает на наличие благоприятных
+                            карм в сознании людей для практики высших освобождающих учений Ануттара-тантры. Весьма важно, что текст, который существовал ранее в виде отдельных
+                             устных наставлений, полностью представлен непосредственно самим Мастером – практиком Лайя-йоги, что исключает ошибки и двусмысленности.">
+                    Подробнее...
+                </button>
+            </div><!--card-block-->
+        </div> <!--card-->
         <div class="card ">
-            <img class="card-img-top book-shadow" src="../public/images/kodeksMastera.jpg" alt="Card image cap">
-            <div class=" card-body ">
-                <p class="card-text"><small>
-                        This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-                    </small></p>
-            </div>
-        </div>
+            <img class="card-img-top book-shadow book-size" src="../public/images/books/000009.jpg" alt="Card image cap">
+            <div class="card-block">
+                <p class="card-text" ><small></small></p>
+                <button type="button" class="btn btn-link" data-toggle="popover" title="Путь божественной гордости. Свабхава Упадеша Чинтамани"
+                        data-content="Путь пестования Божественной Гордости является одним из трех разделов Учения о поддержании созерцания
+                        (Праджня-янтры) в традиции Лайя-йоги. Праджня-Янтра означает раздел Мудрости – это наше созерцание, все, что связано
+                        с осознанностью, с осознаванием. Это Учение включает в себя три раздела: раздел созерцания (раздел ума), когда мы говорим
+                         о самоосознавании, самоосвобождении, обнаженном осознавании; раздел Бхавы или Божественная Гордость и раздел Пространства.
+                          Когда мы практикуем Атма-Вичару – это раздел обнаженного осознавания, когда мы практикуем чистое видение, Бхаджан Мандалу,
+                           Божественную Гордость – это раздел Бхавы; когда мы практикуем Шамбхави-мудру – это техники пространства. Это разделение
+                           условное для удобства изучения, а естественное состояние – оно одно, что бы вы ни практиковали.">
+                    Подробнее...
+                </button>
+            </div><!--card-block-->
+        </div><!--card-->
         <div class="card ">
-            <img class="card-img-top book-shadow" src="../public/images/kodeksMastera.jpg" alt="Card image cap">
-            <div class=" card-body ">
-                <p class="card-text"><small>
-                        This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-                    </small></p>
-            </div>
-        </div>
+            <img class="card-img-top book-shadow book-size" src="../public/images/books/000011.jpg" alt="Card image cap">
+            <div class="card-block">
+                <p class="card-text" ><small></small></p>
+                <button type="button" class="btn btn-link" data-toggle="popover" title="Путь растворения пяти элементов. Панча Бхута Лайя Чинтана"
+                        data-content="Пять элементов – это пять фундаментальных энергий, проявляющихся в бытии.Согласно учению тантры, материя Вселенной
+                        состоит из комбинаций пяти элементов.Пять элементов находятся как вовне, образуя внешнюю Вселенную, так и внутри, образуя тело.
+                        Учение Лайя-йоги заключается в растворении пяти элементов, составляющих физическое тело, до уровня звука и света.">
+                    Подробнее...
+                </button>
+            </div><!--card-block-->
+        </div><!--card-->
         <div class="card ">
-            <img class="card-img-top book-shadow" src="../public/images/kodeksMastera.jpg" alt="Card image cap">
-            <div class=" card-body ">
-                <p class="card-text"><small>
-                        This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-                    </small></p>
-            </div>
-        </div>
+            <img class="card-img-top book-shadow book-size" src="../public/images/books/000023.jpg" alt="Card image cap">
+            <div class="card-block">
+                <p class="card-text" ><small></small></p>
+                <button type="button" class="btn btn-link" data-toggle="popover" title="Анава-йога"
+                        data-content="Пока мы не обнаружили принцип обнаженного осознавания, наш ум сильно схватывается этой реальностью, внешним миром.
+                         Практики Анава-йоги – это эффективные методы, которые позволяют за короткое время освободиться от связанности дуальным мышлением,
+                          двойственными представлениями, обнаружить неконцептуальную осознанность, самоосвободить воздействие внешних объектов и начать
+                           воспринимать мир с точки зрения вечности, бесконечности, парадокса, игры. ">
+                    Подробнее...
+                </button>
+            </div><!--card-block-->
+        </div><!--card-->
         <div class="card ">
-            <img class="card-img-top book-shadow" src="../public/images/kodeksMastera.jpg" alt="Card image cap">
-            <div class=" card-body ">
-                <p class="card-text"><small>
-                        This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-                    </small></p>
-            </div>
-        </div>
-    </div>
-
+            <img class="card-img-top book-shadow book-size" src="../public/images/books/000024.jpg" alt="Card image cap">
+            <div class="card-block">
+                <p class="card-text" ><small></small></p>
+                <button type="button" class="btn btn-link" data-toggle="popover" title="Истина вне ума"
+                        data-content="Пока мы не открыли мировоззрение недвойственности или не научились пребывать в состоянии медитации за пределами мыслей,
+                         концепции имеют силу над нами, мысленные конструкции всегда очаровывают нас, и наш ум подпадает под их влияние. А потому, даже если
+                          мы практикуем различные тантрические методы трансформации, поклонения, начитывание мантр или делаем практики из йога-тантры,
+                          практикуя мудры, пранаямы и концентрацию на чакрах, до тех пор, пока мысли имеют власть над нами, мы продолжаем находиться
+                           в клетке индивидуального «я», так и не покидая кокон личностного восприятия Бытия. Практика самоосвобождения с опорой на
+                           ум позволяет нам обесценить мысленные построения и проникнуть в то, что находится за пределами ума.">
+                    Подробнее...
+                </button>
+            </div><!--card-block-->
+        </div><!--card-->
+    </div><!--card-deck-->
     </div><!-- /.container -->
 
 
+    <div class="  container py-3 bg-faded">
+        <div class="card-deck">
+            <div class="card ">
+                <img class="card-img-top book-shadow book-size"  src="../public/images/books/000027.jpg" alt="Card image cap">
+                <div class="card-block">
+                    <p class="card-text" ><small></small></p>
+                    <button type="button" class="btn btn-link" data-toggle="popover" title="Феномен Гуру-йоги"
+                            data-content="Гуру-йога есть авторитетный принцип духовного обучения, основанный на установлении особых священных взаимоотношений
+                             между Учителем и учеником. Обычно о Гуру-йоге говорят в контексте тантрических учений индуизма или буддизма, однако феномен
+                             Гуру-йоги распространен гораздо более широко, нежели принято об этом думать, и присутствует внутри любой серьезной духовной традиции.">
+                        Подробнее...
+                    </button>
+                </div><!--card-block-->
+            </div><!--card-->
+            <div class="card ">
+                <img class="card-img-top book-shadow book-size" src="../public/images/books/8950.jpg" alt="Card image cap">
+                <div class="card-block">
+                    <p class="card-text" ><small></small></p>
+                    <button type="button" class="btn btn-link" data-toggle="popover" title="Даттатрейя как Учитель человечества"
+                            data-content="Бхагаван Даттатрейя есть Учитель всех Учителей, изначальный Наставник всех йогов и сиддхов. Он олицетворяет принцип
+                            воззрения Адвайты и самоосвобожденного поведения – игры в облике авадхуты. Даттатрейя является воплощением Троицы богов (Тримурти):
+                             Творца, Хранителя и Разрушителя (Брахмы, Вишну и Шивы), которые воплотились в семье риши Атри и Анасуи, дав им такое благословение
+                              после того, как Анасуя своей духовной силой преподала им урок чистоты и смирения. Он не просто является воплощением Троицы – он
+                               олицетворяет Путь и цель самой абсолютной Истины или Реальности. Он является самой Истиной.">
+                        Подробнее...
+                    </button>
+                </div><!--card-block-->
+            </div><!--card-->
+            <div class="card ">
+                <img class="card-img-top book-shadow book-size" src="../public/images/books/9376.jpg" alt="Card image cap">
+                <div class="card-block">
+                    <p class="card-text" ><small></small></p>
+                    <button type="button" class="btn btn-link" data-toggle="popover" title="Книга о великом сиддха-йогине Свами Брахмананде"
+                            data-content="Авадхута – это тот, кто полностью преодолел двойственность материального мира, «я» которого полностью стало единым с
+                            абсолютным Источником Бытия. Авадхуты, освободившись от всех мирских страданий и обязанностей, живут и действуют, ни к чему не привязываясь
+                             и ни о чём не беспокоясь. Они действуют «не действуя», проявляя свое запредельное состояние в относительном измерении. Это чистые и простые,
+                              как дети, личности. Авадхута не отождествляет себя со своим телом и умом – он достиг наиболее чистого уровня сознания, доступного
+                              индивидуальной душе (дживе) в человеческом воплощении. ">
+                        Подробнее...
+                    </button>
+                </div><!--card-block-->
+            </div><!--card-->
+            <div class="card ">
+                <img class="card-img-top book-shadow book-size" src="../public/images/books/defaultBook.png" alt="Card image cap">
+                <div class="card-block">
+                    <p class="card-text" ><small></small></p>
+                    <button type="button" class="btn btn-link" data-toggle="popover" title=""
+                            data-content="">
+
+                    </button>
+                </div><!--card-block-->
+            </div><!--card-->
+            <div class="card ">
+                <img class="card-img-top book-shadow book-size" src="../public/images/books/defaultBook.png" alt="Card image cap">
+                <div class=" card-body ">
+
+                    <p class="card-text"><small>
+
+                        </small></p>
+                </div><!--card-block-->
+            </div><!--card-->
+        </div><!--card-deck-->
+    </div><!-- /.container -->
 
 
 </div>
