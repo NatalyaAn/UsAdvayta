8229db7685fb98c5e1e7625fae1684866e117f95
NatalyaAn <nata.an@gmail.com>
2017-12-19 20:30:57 -0500

    news changed

--- variables/varNews.php
+++ variables/varNews.php
@@ -31,11 +31,11 @@ $news = '
    <div class="card p-2 card-shadow">
     <div class="card-block">
     <small class="text">
-      <h4 class="card-title">9-10 декабря</h4>
-      <h6 class="card-title">Семинар «Управления внутренней энергией в традиции йоги сиддхов» </h6>
+      <h4 class="card-title">20 декабря</h4>
+      <h6 class="card-title">Лекция «Практика медитации» </h6>
 
-<p class="card-text">Занятия состоят из чередующихся теоретических знаний и усвоения через практику. Практикум по крийя-кундалини-йоге. Балансировка негармоничной и болезненной энергии.</p>
-<p class="card-text font-weight-bold">Время: с 2-6 pm (суббота, воскресенье)</p>
+<p class="card-text">Раманатха продолжит раскрывать начатую 15-го декабря тему о медитации.</p>
+<p class="card-text font-weight-bold">Время: с 7-9 pm (среда)</p>
 <p class="card-text font-weight-bold">Место: Brooklyn, 365 avenue U, 2-nd floor.</p>
       <footer>
       <a href="#" class="card-link"></a>
@@ -46,64 +46,43 @@ $news = '
   <div class="card p-2 card-shadow">
     <div class="card-block ">
      <small class="text">
-      <h4 class="card-title">15 декабря</h4>
-      <h6 class="card-title">Обучающий тренинг «Искусство медитации»</h6>
-  <p class="card-text">
-      В программе:</p>
-<p class="card-text">Положения тела при медитации сидя и в ходьбе,
- положение глаз в медитации,
- ключевые точки в медитации,
- как подобрать объект для практики,
- как выстроить стратегию индивидуальной практики,
- какие этапы и знаки на пути,
- как разобраться с целью медитативной практики,
- как устранить препятствия,
- как остановить ум.</p>
- <p class="card-text font-weight-bold">Время: с 7-9 pm (пятница)</p>
-<p class="card-text font-weight-bold">Место проведения: Brooklyn, 365 avenue U, 2-nd floor.</p>
-
-      <footer>
-      <a href="#" class="card-link"></a>
-      </footer>
-     </small>
-    </div>
-  </div><!-- /.card -->
-
-
-
-  <div class="card p-2 card-shadow">
-    <div class="card-block ">
-     <small class="text">
-      <h4 class="card-title">16-18 декабря</h4>
-      <h6 class="card-title">Трехдневный ритрит «Концентрация. Медитация. Созерцание»</h6>
-
-<p class="card-text">16-е — Концентрация — основная часть практик будет направлена на развитие концентрации ума, полноты внимательности.</p>
-<p class="card-text">17-е — Медитация — большинство практик будут связаны с расширением восприятия, раскрытием интуиции.</p>
-<p class="card-text">18-е — Созерцание — основная часть практик будет призвана раскрыть естественное созерцательное состояние сознания, а именно — без цепляния за сформированный опыт, свободное от надежд и страхов.</p>
-<p class="card-text font-weight-bold">Место проведения: NJ, Roosevelt</p>
-      <footer>
-      <a href="#" class="card-link"></a>
-      </footer>
-     </small>
-    </div>
-  </div><!-- /.card -->
-  <div class="card p-2 card-shadow">
-    <div class="card-block ">
-     <small class="text">
       <h4 class="card-title">21 декабря</h4>
-      <h6 class="card-title">Лекция «Карма и Дхарма»</h6>
-      <p class="card-text">
-      Беседа о карме и путях выхода из нее. Предельно простыми словами лекция для тех, кто хочет прояснить для себя базовые жизненные вопросы:
- причина и следствия, смысл жизни, йога, Дхарма, духовный Учитель, реинкарнация.</p>
-
- <p class="card-text font-weight-bold">Время: с 5-7:30 pm (четверг)</p>
+      <h6 class="card-title">Открытая беседа о карме и путях выхода из нее. </h6>
+  <p class="card-text">
+     Предельно простыми словами лекция для тех, кто хочет прояснить для себя базовые жизненные вопросы:</p>
+причина и следствия, смысл жизни,йога, дхарма, духовный учитель, реинкарнация.
+ <p class="card-text font-weight-bold">Время: с 5-7:30 pm (пятница)</p>
 <p class="card-text font-weight-bold">Место проведения: Brooklyn, 365 avenue U, 2-nd floor.</p>
+
       <footer>
       <a href="#" class="card-link"></a>
       </footer>
      </small>
     </div>
   </div><!-- /.card -->
+
+
+
+  <div class="card p-2 card-shadow">
+    <div class="card-block ">
+     <small class="text">
+      <h4 class="card-title">23 декабря</h4>
+      <h6 class="card-title">Хаван</h6>
+
+<p class="card-text">Санньяси Раманатха и Лакшита приглашают всех желающих присоединиться к ритуальной практике призывания благословения Даттатрейи через огненное подношение (хаван, ягья).</p>
+
+<p class="card-text font-weight-bold">Время: с 11ам-1рм (суббота)</p>
+<p class="card-text font-weight-bold">Время: с 2рм Общий прасад, общение с духовными братьями и сестрами.</p>
+<p class="card-text font-weight-bold">Место проведения: Elmwood Park, NJ 07407</p>
+
+
+      <footer>
+      <a href="#" class="card-link"></a>
+      </footer>
+     </small>
+    </div>
+  </div><!-- /.card -->
+
 
 </div> <!-- /.d-flex -->
 </div><!-- /.collapse -->
